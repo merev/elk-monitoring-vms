37e0093889dd5bfc316ceafa317bae3ec91cd1a7
merev <101055426+merev@users.noreply.github.com>
2023-08-16 11:44:47 +0300

    logstash config for the beats added

--- initial-config/logstash_install.sh
+++ initial-config/logstash_install.sh
@@ -6,6 +6,9 @@ wget https://artifacts.elastic.co/downloads/logstash/logstash-8.9.0-amd64.deb
 echo "* Install the software"
 dpkg -i logstash*.deb
 
+echo "* Add beats configuration to logstash"
+cp /shared/logstash/heartbeat /etc/logstash/conf.d/beats.conf
+
 echo "* Start and enable logstash.service"
 systemctl daemon-reload
 systemctl enable --now logstash
